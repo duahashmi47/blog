21c576d9ff61569636157cc8b837efe938a8ad7d
duahashmi47 <duahashmi47@gmail.com>
2026-01-31 23:15:50 +0500

    Added post details and comments functionality

--- blog/crashblog/models.py
+++ blog/crashblog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_at',]
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- blog/crashblog/views.py
+++ blog/crashblog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import CommentForm
 
-# Create your views here.
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        form = CommentForm()
+    return render(request, 'crashblog/detail.html', {'post': post, 'form': form})
+
