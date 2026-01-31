9e943df77c63e6448860e7c7801b4b6c32a368df
duahashmi47 <duahashmi47@gmail.com>
2026-01-31 21:41:14 +0500

    Created the crash blog app

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import frontpage, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', frontpage, name='frontpage'),
+    path('about/', about, name='about'),
 ]
